975c3f905e14f830266b68309ce3dd533fecb98c
Papa_D <zibezt@mail.ru>
2024-12-04 22:06:39 +0800

    домашняя работа 23, реализованы все запросы

--- home_23/app.py
+++ home_23/app.py
@@ -1,21 +1,44 @@
 import os
 
-from flask import Flask, jsonify, request
+from flask import Flask, request
 
 app = Flask(__name__)
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 DATA_DIR = os.path.join(BASE_DIR, "data")
 
-commands = {"filter": lambda data, value: filter(lambda line: value in line, data)}
+
+def filter_query(data, value):
+    return filter(lambda line: value in line, data)
+
+
+def map_query(data, value):
+    return map(lambda line: line.split()[int(value)], data)
+
+
+def unique_query(data, value):
+    return iter(set(data))
+
+
+def sort_query(data, value):
+    return iter(sorted(data, reverse=value == "desc"))
+
+
+def limit_query(data, value):
+    return (line for i, line in enumerate(data) if i < int(value))
+
+
+commands = {
+    "filter": filter_query,
+    "map": map_query,
+    "unique": unique_query,
+    "sort": sort_query,
+    "limit": limit_query,
+}
 
 
 @app.route("/perform_query", methods=["POST"])
 def perform_query():
-    # получить параметры query и file_name из request.args, при ошибке вернуть ошибку 400
-    # проверить, что файла file_name существует в папке DATA_DIR, при ошибке вернуть ошибку 400
-    # с помощью функционального программирования (функций filter, map), итераторов/генераторов сконструировать запрос
-    # вернуть пользователю сформированный результат
     cmd1 = request.json.get("cmd1")
     value1 = request.json.get("value1")
     cmd2 = request.json.get("cmd2")
@@ -37,8 +60,14 @@ def perform_query():
 
         if cmd1 in commands:
             data = commands[cmd1](data, value1)
-
-        elif cmd2 in commands:
+        else:
+            return (
+                app.response_class(
+                    f"Неизвестная конанда {cmd1}", content_type="text/plain"
+                ),
+                400,
+            )
+        if cmd2 in commands:
             data = commands[cmd2](data, value2)
         else:
             return (
@@ -48,8 +77,8 @@ def perform_query():
                 400,
             )
 
-        result = list(data)
-        return jsonify(result)
+        result = "\n".join(data)
+        return app.response_class(result, content_type="text/plain")
     except Exception as e:
 
         return app.response_class(f"Ошибка: {str(e)}", content_type="text/plain"), 500
